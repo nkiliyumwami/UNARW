beea09e98bf73bc5e483044c92de3854d574f022
Aline096 <anoclez@gmail.com>
2024-06-04 12:37:23 +0200

    Adding content

--- components/Sections/GetInvolved.tsx
+++ components/Sections/GetInvolved.tsx
@@ -27,9 +27,9 @@ const GetInvolvedSection = () => {
   return (
     <section className="py-12 bg-gradient-to-r from-purple-400 to-[#4894DF]">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        {/* <h2 className="text-3xl font-extrabold text-gray-900 text-center mb-8">
+        <h2 className="text-3xl font-extrabold text-gray-900 text-center mb-8">
           Get Involved
-        </h2> */}
+        </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           {involvementOptions.map((option, index) => (
             <div
