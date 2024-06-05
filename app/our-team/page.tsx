49c6cc4d0f9699e2cb4f578e6d53c865e82edb4d
Aline096 <anoclez@gmail.com>
2024-06-05 16:20:57 +0200

    Our Team Page

---
+++ app/our-team/page.tsx
@@ -0,0 +1,19 @@
+import OurTeam from '@/components/Sections/TeamSection'
+import React from 'react'
+import bgImage from '../../public/team.jpeg'
+
+const Team = () => {
+  return (
+    <div>
+      <div
+        className="py-32 h-[256px] bg-center bg-cover bg-no-repeat relative"
+        style={{ backgroundImage: `url(${bgImage.src})` }}
+      >
+        <div className="absolute inset-0 bg-black bg-opacity-50 backdrop-blur-sm"></div>
+      </div>
+      <OurTeam />
+    </div>
+  )
+}
+
+export default Team
